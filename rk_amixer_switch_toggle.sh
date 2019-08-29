1cc59987064888aae7e50b074f4f2a3fe5a0c6db
Fenrir <fenrir.lin@rock-chips.com>
2019-08-29 18:12:04 +0800

    Cut and modify, and add dynamic acquisition, independent testing, and restore field functions

--- rk_amixer_switch_toggle.sh
+++ rk_amixer_switch_toggle.sh
@@ -1,7 +1,8 @@
-#! /bin/sh
+#! /bin/bash
 # 
 # Copyright (C) 2011 Texas Instruments Incorporated - http://www.ti.com/
-#  
+# Copyright (C) 2019 Fuzhou Rockchip Electronics Co.,Ltd
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as 
 # published by the Free Software Foundation version 2.
@@ -11,22 +12,35 @@
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
-# @desc Toggles the switch by playing the audio in backgroung using amixer interface.
+# @desc Toggles the switch by playing the audio in backgroung using amixer.
 # @params  l) TEST_LOOP    test loop for switch toggling. default is 1.
-# @history 2011-04-07: First version
-# @history 2011-05-13: Removed st_log.sh
-source "../common/common.sh"  # Import do_cmd(), die() and other functions
+#          D) <device>     audio device to use during the test,The default is hw:0,0
 
 ############################# Functions #######################################
 usage()
 {
-	cat <<-EOF >&2
-	usage: ./${0##*/} [-l TEST_LOOP]  [-D <device> ]
-  -D audio device to use during the test, i.e hw:1,0, defaults to hw:0,0
+        cat <<-EOF >&2
+	usage: bash ${0##*/} [-l TEST_LOOP]  [-D <device> ]
+        -D audio device to use during the test, i.e hw:1,0, defaults to hw:0,0
 	EOF
 	exit 0
 }
 
+##print log and execute cmd
+do_cmd() 
+{
+	CMD=$*
+	echo -e "\n|LOG|CMD=$CMD"
+	eval $CMD
+	RESULT=$?
+	if [ $RESULT -ne 0 ];then
+		echo "|FAIL|:$CMD failed. Return code is $RESULT"
+		exit $RESULT
+	fi
+	if [ $RESULT -eq 0 ];then
+		echo "|PASS|:$CMD passed."
+	fi
+}
 ################################ CLI Params ####################################
 # Please use getopts
 while getopts  :l:D:h arg
@@ -39,94 +53,70 @@ do case $arg in
 esac
 done
 
+#Use aplay to get information by default
+PLAYBACK_SOUND_INFO="$(aplay -l | grep -i card)"
+
+PLAYBACK_SOUND_CARDS=( $(aplay -l | grep -i card | cut -d , -f 1 | grep -o '[0-9]\+:' | cut -c 1) )
+PLAYBACK_SOUND_DEVICE=($(aplay -l | grep -i card | cut -d , -f 2 | grep -o '[0-9]\+:' | cut -c 1) )
+
+
+
 # Define default values if possible
-: ${TEST_LOOP:=3}
-: ${DEVICE:=$(get_audio_devnodes.sh -d aic -t play | grep 'hw:[0-9]' || echo 'hw:0,0')}
+: ${TEST_LOOP:=1}
+
+#If the -D parameter is not used. Default test the first sound card obtained by aplay
+: ${DEVICE:="hw:${PLAYBACK_SOUND_CARDS},${PLAYBACK_SOUND_DEVICE}"}
+
 CARD=$(echo "${DEVICE}" | cut -c 4)
 
-############################ USER-DEFINED Params ###############################
-# Try to avoid defining values here, instead see if possible
-# to determine the value dynamically. ARCH, DRIVER, SOC and MACHINE are 
-# initilized and exported by runltp script based on platform option (-P)
-case $ARCH in
-esac
-case $DRIVER in
+########################### DO WORK ###############################
 
-esac
-case $SOC in
-esac
-case $MACHINE in
-*dra7xx-evm|am57xx-evm|k2g-evm)
-	CAPTURE_SWITCH_NAME_1="PGA Capture Switch";
-	PLAYBACK_SWITCH_NAME_1="Line Playback Switch";
-	PLAYBACK_SWITCH_NAME_2="HP Playback Switch"
-	;;
-am180x-evm|dm355-evm|dm365-evm|dm6446-evm|dm6467-evm|dm368-evm)
-	CAPTURE_SWITCH_NAME_1="PGA Capture Switch";
-	PLAYBACK_SWITCH_NAME_1="LineL Playback Switch"
-	PLAYBACK_SWITCH_NAME_2="LineR Playback Switch"
-	;;
-omap3evm)	
-	CAPTURE_SWITCH_NAME_1="Analog Right AUXR Capture Switch";
-	CAPTURE_SWITCH_NAME_2="Analog Left AUXL Capture Switch";	
-	PLAYBACK_SWITCH_NAME_1="DAC2 Analog Playback Switch"
-	amixer cset numid=25 1,1
-	amixer cset numid=28 1,1	
-	;;
-am37x-evm|beagleboard)
-	CAPTURE_SWITCH_NAME_1="Analog Right AUXR Capture Switch";
-	CAPTURE_SWITCH_NAME_2="Analog Left AUXL Capture Switch";	
-	PLAYBACK_SWITCH_NAME_1="DAC2 Analog Playback Switch"
-	;;
-am3517-evm)
-	CAPTURE_SWITCH_NAME_1="Digital Capture Switch";
-	PLAYBACK_SWITCH_NAME_1="Digital Playback Switch"
-	;;	
-da850-omapl138-evm)
-	CAPTURE_SWITCH_NAME_1="PGA Capture Switch";
-	PLAYBACK_SWITCH_NAME_1="Line Playback Switch"
-	;;	
-am387x-evm|am389x-evm|am335x-evm|dm385-evm|am43xx-gpevm)
-        CAPTURE_SWITCH_NAME_1="PGA Capture Switch";
-        PLAYBACK_SWITCH_NAME_1="HP Playback Switch"
-        ;;
-am43xx-epos)
-        CAPTURE_SWITCH_NAME_1="ADC Capture Switch";
-        PLAYBACK_SWITCH_NAME_1="HP Driver Playback Switch"
-        PLAYBACK_SWITCH_NAME_2="Speaker Driver Playback Switch"
-        ;;
-esac
+#amixer -c ${CARD} controls  #More detailed but may not be necessary
+echo "==========================Switch Infomation=========================="
+amixer -c ${CARD} contents | grep Switch 
 
-########################### REUSABLE TEST LOGIC ###############################
+echo "==========================Sound Infomation=========================="
+echo -e "Sound_Info:\n$PLAYBACK_SOUND_INFO" #actually is Playback_Sound_Info
+echo "The number of test_loop is $TEST_LOOP"
+echo "Test device is $DEVICE"
+echo "Test card is $CARD"
 
-amixer -c ${CARD} controls
-amixer -c ${CARD} contents
+NUMBER_OF_SWITHCES=( $(amixer -c ${CARD} contents | grep Switch | cut -d = -f 4 | wc -l) )
+echo "Number of Switches is $NUMBER_OF_SWITHCES"
+
+echo "==========================Start Test=========================="
+
+#Start testing at the back without changing the switch
 arecord -D ${DEVICE} -f dat -d 300 | aplay -D ${DEVICE} -f dat -d 300&
+sleep 3 # There is a delay in calling the audio device
 
 i=0
 while [[ $i -lt $TEST_LOOP ]]
 do
-	do_cmd amixer -c ${CARD} cset name=\'$CAPTURE_SWITCH_NAME_1\' 0
-	if [ "$CAPTURE_SWITCH_NAME_2" != "" ] ; then
-		do_cmd amixer -c ${CARD} cset name=\'$CAPTURE_SWITCH_NAME_2\' 0
-	fi
-	sleep 15
-	do_cmd amixer -c ${CARD} cset name=\'$CAPTURE_SWITCH_NAME_1\' 1
-	if [ "$CAPTURE_SWITCH_NAME_2" != "" ] ; then
-		do_cmd amixer -c ${CARD} cset name=\'$CAPTURE_SWITCH_NAME_2\' 1
-	fi
-	sleep 15
-	
-	do_cmd amixer -c ${CARD} cset name=\'$PLAYBACK_SWITCH_NAME_1\' 0
-	if [ "$PLAYBACK_SWITCH_NAME_2" != "" ] ; then
-		do_cmd amixer -c ${CARD} cset name=\'$PLAYBACK_SWITCH_NAME_2\' 0
-	fi
-	sleep 15
-	do_cmd amixer -c ${CARD} cset name=\'$PLAYBACK_SWITCH_NAME_1\' 1
-	if [ "$PLAYBACK_SWITCH_NAME_2" != "" ] ; then
-		do_cmd amixer -c ${CARD} cset name=\'$PLAYBACK_SWITCH_NAME_2\' 1
-	fi
-	sleep 15	
+	j=0
+        while [[ $j -lt 3 ]]
+        do
+                SWITCH_NAME="$(amixer -c ${CARD} contents | grep Switch | cut -d = -f 4 | awk 'FNR=='$j+1'')" #awk star from 1 not 0
+                echo "================$SWITCH_NAME Test================"
+
+                #Get the initial value of the switch
+                SWITCH_VALUES=$(eval amixer cget name=$SWITCH_NAME | grep values=1 | cut -d , -f 3 | cut -d = -f 2)
+                echo -e "|LOG|The initial value of $SWITCH_NAME is $SWITCH_VALUES.\n"
+
+                if [ $SWITCH_VALUES == 1 ];then
+                        do_cmd amixer -c ${CARD} cset name=$SWITCH_NAME 0
+                        sleep 5
+                        do_cmd amixer -c ${CARD} cset name=$SWITCH_NAME 1
+                        sleep 5
+                fi
+                if [ $SWITCH_VALUES == 0 ];then
+                        do_cmd amixer -c ${CARD} cset name=$SWITCH_NAME 1
+                        sleep 5
+                        do_cmd amixer -c ${CARD} cset name=$SWITCH_NAME 0
+                        sleep 5
+                fi
+                let "j += 1"
+        done
 	let "i += 1"	
 done
 
